748bf48a1304e7006152125a00fc69a1c05b48d3
Seokhyun Bae <seokhyun_bae@tmax.co.kr>
2021-01-13 12:23:48 +0900

    feat: JPA 및 H2 Dependency 설정
    - Hibernate 및 H2 Database 사용을 위한 Dependency 추가

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+}
+
+group = "jpaBasic"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+    implementation("org.hibernate", "hibernate-gradle-plugin", "5.3.10.Final")
+    implementation("com.h2database", "h2", "1.4.200")
+}
