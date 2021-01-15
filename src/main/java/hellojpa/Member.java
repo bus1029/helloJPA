f441ecf295b5b24ab45071e1947ee1311d49521f
Seokhyun Bae <seokhyun_bae@tmax.co.kr>
2021-01-15 15:19:18 +0900

    feat: Column 및 SequenceGenerator 실습
    - SequenceGenerator로 직접 Sequence 생성하여 Member의 ID 부여하도록 수정
    - Temporal, Enumerated 사용하여 JPA에서 제공하는 DDL 생성 기능 테스트

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,33 +1,53 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 // Entity를 명시해줘야 JPA에서 관리해줌
 @Entity
 //  테이블 이름을 명시해줄 수 도 있음
 //@Table(name = "USER")
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 1
+)
 public class Member {
   // Primary Key
   @Id
+/*  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;*/
+
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
   private Long id;
+
   // 컬럼 이름도 따로 명시 가능
-//  @Column(name = "username")
-  private String name;
+  @Column(name = "name")
+  private String username;
 
-  public Long getId() {
-    return id;
-  }
+  private Integer age;
 
-  public String getName() {
-    return name;
-  }
+  // Enum을 쓰고 싶다!
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+  // DATE, TIME, TIMESTAMP(DATE + TIME)
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
 
-  public void setName(String name) {
-    this.name = name;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+
+  // VARCHAR를 넘는 큰 컨텐츠를 넣고 싶을 때
+  // 문자 타입이기 때문에 CLOB
+  @Lob
+  private String description;
+
+  // 컬럼 매핑이 안됨, 메모리에서만 사용
+  @Transient
+  private int tmp;
+
+  public Member() {
+
   }
 }
