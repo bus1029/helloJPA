d1661f74adbe946c8057c2c7576cc4f18996ef35
Seokhyun Bae <seokhyun_bae@tmax.co.kr>
2021-01-22 19:35:16 +0900

    feat: 상속관계에서 JOIN 전략을 채택했을 때의 예시 작성
    - 상속관계에서 JOIN 전략을 선택했을 때의 예시 작성

---
+++ src/main/java/jpa/inheritance/Album.java
@@ -0,0 +1,8 @@
+package jpa.inheritance;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Album extends Item {
+  private String artist;
+}
---
+++ src/main/java/jpa/inheritance/Book.java
@@ -0,0 +1,9 @@
+package jpa.inheritance;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends Item {
+  private String author;
+  private String isbn;
+}
