1235f6d917204c547901e9a3e76e98e227eb8d4a
Seokhyun Bae <bus1029@gmail.com>
2021-01-25 22:30:00 +0900

    feat: build.gradle 수정 및 .gitignore에 out 폴더 추가
    - build.gradle.kts에 UTF-8로 컴파일 될 수 있도록 수정
    - .gitignore에 out 폴더 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 group = "jpaBasic"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
 }
@@ -14,3 +15,10 @@ dependencies {
     implementation("org.hibernate", "hibernate-gradle-plugin", "5.3.10.Final")
     implementation("com.h2database", "h2", "1.4.200")
 }
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+}
+
